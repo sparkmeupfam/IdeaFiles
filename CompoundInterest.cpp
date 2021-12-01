c4e9b490dbba5874d68580133696a2373e50be60
Dont Care <suckmyspamemail@gmail.com>
2021-11-30 20:16:11 -0600

    Just another commit

---
+++ CompoundInterest.cpp
@@ -0,0 +1,13 @@
+// The idea behind this program is to calculate compound interest
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "checking the process" << endl;
+
+return 0;
+
+}
