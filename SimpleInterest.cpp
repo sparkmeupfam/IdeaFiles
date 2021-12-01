b37b0c138c061674a2daacff8ef1aa97983488e6
Dont Care <suckmyspamemail@gmail.com>
2021-11-30 20:12:13 -0600

    adding new files

---
+++ SimpleInterest.cpp
@@ -0,0 +1,29 @@
+// The Idea behind this program is to calculate simple interest via terminal.
+
+#include <iostream>
+
+using namespace std;
+
+int calc(int principle){
+
+int total = 0;
+
+total = 2*principle;
+
+return total;
+
+}
+
+int main(){
+
+int principle, total = 0;
+
+cout << "Insert the initial amount: " << endl;
+
+cin >> principle;
+
+cout << "The total amount is: " << calc(principle) << endl;
+
+return 0;
+
+} 
